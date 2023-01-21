f92add734a1fe421df861e7992c7484d5ff8c772
JoniMeichtry11 <joni.meichtry@gmail.com>
2023-01-21 00:31:03 -0300

    fix: PORT environment

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const {errorHandler, logErrors, boomErrorHandler} = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// const whitelist = ['http://localhost:4200', 'https://myapp.co'];
-// const options = {
-//   origin: (origin, callback) => {
-//     if(whitelist.includes(origin)){
-//       callback(null, true);
-//     } else {
-//       callback(new Error('no permitido'))
-//     }
-//   }
-// }
-// app.use(cors(options));
+const whitelist = ['http://localhost:4200', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)){
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'))
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
-  res.send("Hola, mi server en express");
+  res.send("Hola, este es mi server en express");
 });
 
 app.get('/nueva-ruta', (req, res) => {
