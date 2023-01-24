d5e72ea748aed779cc2b7bcc8e8e47edb8bbf3f3
JoniMeichtry11 <joni.meichtry@gmail.com>
2023-01-24 05:47:35 -0300

    fix: fix files

--- services/productService.js
+++ services/productService.js
@@ -1,11 +1,15 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
+const pool = require('../libs/postgres.pool');
+
 class ProductService {
 
   constructor(){
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.log(err));
   }
 
   async generate(){
@@ -31,7 +35,9 @@ class ProductService {
   }
 
   async find(){
-    return this.products;
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
+    return rta.rows;
   }
 
   async findOne(id){
